d4fc12d532cdb61ddcdfdc920a80bc4036063c09
Seongwon <sngoneee@gmail.com>
2023-11-14 18:01:11 +0900

    gitignore 설정

---
+++ Hello/src/Hello.java
@@ -0,0 +1,13 @@
+
+
+
+
+public class Hello {
+
+	public static void main(String[] args) {
+		String s = "12345";
+		int i = Integer.parseInt(s);
+		System.out.printf("문자를 숫자로 %d   (%d)   \n", i , i+10);
+	}
+
+}
