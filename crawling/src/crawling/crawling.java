0570048571da7163a17a74fd269e415c65e32a66
Seongwon <sngoneee@gmail.com>
2023-11-20 13:43:16 +0900

    수정

--- crawling/src/crawling/crawling.java
+++ crawling/src/crawling/crawling.java
@@ -31,7 +31,7 @@ public class crawling {
 		String K15_short_dist = null;														 //가장 가까운 곳의 변수 초기화
 		String K15_long_dist = null;														 //가장 먼 곳의 변수 초기화
 		
-		
+		ㄴㅇㄹㄴㅇㄹ
 		
 		int K15_LineCnt = 0;																 //반복하는 횟수 0으로 초기화								 		
 		while((K15_readtxt = K15_br.readLine()) != null) {									 //내용이 null이 아니면 계속 반복
